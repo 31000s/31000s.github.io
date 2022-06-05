efac1e84d1fd3b7601d5616317dd4042b0738596
burakyabgu <64856839+burakyabgu@users.noreply.github.com>
2022-06-05 19:23:57 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typing', {
-    strings: ["Coder", "Developer", "Designer"],
+    strings: ["Coder", "Developer", "Data scientist", "Designer"],
     loop: true,
     typeSpeed: 65,
     backSpeed: 65
